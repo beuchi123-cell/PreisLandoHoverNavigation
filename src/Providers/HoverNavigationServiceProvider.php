8adbe121fbbf5a7ffc5ef565ad31f4031c61cffc
beuchi123-cell <beuchi123@googlemail.com>
2026-08-12 16:07:25 +0200

    Update HoverNavigationServiceProvider.php

--- src/Providers/HoverNavigationServiceProvider.php
+++ src/Providers/HoverNavigationServiceProvider.php
@@ -2,14 +2,34 @@
 
 namespace PreisLandoHoverNavigation\Providers;
 
+use IO\Helper\ResourceContainer;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
 
 class HoverNavigationServiceProvider extends ServiceProvider
 {
-    /**
-     * Register the service provider.
-     */
+    const PRIORITY = 0;
+
     public function register()
     {
+    }
+
+    public function boot(Twig $twig, Dispatcher $eventDispatcher)
+    {
+        $eventDispatcher->listen(
+            "IO.Resources.Import",
+            function (ResourceContainer $container)
+            {
+                $container->addScriptTemplate(
+                    "PreisLandoHoverNavigation::content.HoverNavigationScript"
+                );
+
+                $container->addStyleTemplate(
+                    "PreisLandoHoverNavigation::content.HoverNavigationStyle"
+                );
+            },
+            self::PRIORITY
+        );
     }
 }

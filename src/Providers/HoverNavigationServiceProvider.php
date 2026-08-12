50982627a519fe95f48d69347d5c3c6ebdc1411d
beuchi123-cell <beuchi123@googlemail.com>
2026-08-12 16:03:53 +0200

    Create HoverNavigationServiceProvider.php

---
+++ src/Providers/HoverNavigationServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PreisLandoHoverNavigation\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class HoverNavigationServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+    }
+}
